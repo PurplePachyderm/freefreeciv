6290349d9d23c015ad09f6225e716ab9b2259bc1
Aurélien Delval <32074290+PurplePachyderm@users.noreply.github.com>
2018-01-02 00:50:48 +0100

    Define map, resources classes

--- include/game/game.h
+++ include/game/game.h
@@ -9,8 +9,13 @@
 #define CITY 0;
 #define BARRACKS 1;
 
+    //Resources types
+#define GOLD 0;
+#define WOOD 1;
+
 
         //Structures
+    //Player
 typedef struct{
     int owner;  //== player.id
     int type;   //See macros
@@ -31,6 +36,21 @@ typedef struct{
 
 typedef struct{
     int id;
+
     building * buildings;   //Array
     unit * units;   //Array
+
+    int gold;
+    int wood;
 }player;
+
+    //Map
+typedef struct{
+    int type;
+    coord pos;
+}resource;
+
+typedef struct{
+    int size;
+    resource * resources;   //Array
+}map;
