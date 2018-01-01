c2da52476b7d10449d7c02fd55c699925cb8f4e4
Aurélien Delval <32074290+PurplePachyderm@users.noreply.github.com>
2018-01-01 23:39:37 +0100

    Now blitting sprites for background, optimization

--- map_display.c
+++ map_display.c
@@ -25,30 +25,44 @@ void blitSprite(SDL_Renderer * renderer,  SDL_Surface * sprites, SDL_Texture * t
 }
 
 
+	//Blits water sprites all over the screen
+void dispBackground(SDL_Window * window, SDL_Renderer * renderer, SDL_Surface * sprites, SDL_Texture * texture, view camera){
+	int width = (int) TILE_SIZE*camera.zoom + 1;
+	coord currentPos;
 
-void dispMap(SDL_Window * window, SDL_Renderer * renderer, SDL_Surface * sprites, SDL_Texture * texture, view camera){
+		//Blitting starts on top left corner, and is done line per line, stops when reaching of screen
+	currentPos.y = camera.offset.y%width - width;	//Relative to camera offset, so it's aligned with continent tiles
+	while(currentPos.y < SCREEN_HEIGHT){
+
+		currentPos.x = camera.offset.x%width - width;
+		while(currentPos.x < SCREEN_WIDTH){
+
+			width = (int) TILE_SIZE*camera.zoom + 1;	//The +1 avoids 1 pixel gap between tiles on particular zoom values
+
+				//if statement avoids blitting sprites under the continent
+			if( (currentPos.x < camera.offset.x+width || currentPos.x > camera.offset.x+(MAP_SIZE-2)*width) ||
+			((currentPos.y < camera.offset.y+width || currentPos.y > camera.offset.y+(MAP_SIZE-2)*width)) ){
+				blitSprite(renderer, sprites, texture, 2, 25, currentPos.x, currentPos.y, width);
+			}
+
+			currentPos.x += width;
+		}
+		currentPos.y += width;
+	}
+}
+
+
+	//Displays sea+continent
+void dispContinent(SDL_Window * window, SDL_Renderer * renderer, SDL_Surface * sprites, SDL_Texture * texture, view camera){
 	int x;
 	int y;
 	int width;
-
-		//Clear Screen
-	SDL_Rect rect;
-	SDL_SetRenderDrawColor(renderer, 66, 229, 244, 255);
-	setRectangle(&rect, 0, 0, 1920, 1080);
-	SDL_RenderFillRect(renderer, &rect);
 
 		//Draw tiles
 	coord sprite;	//Defines which sprite is gonna be used (see sprites reference sheet)
 	for(int i = 0; i<MAP_SIZE; i++){
         for(int j = 0; j<MAP_SIZE; j++){
 
-				//For coasts and corners
-			if(i == 0 || j==0 || i == MAP_SIZE-1 || j == MAP_SIZE-1){
-				x = (int) i*(TILE_SIZE*camera.zoom) + camera.offset.x;
-				y = (int) j*(TILE_SIZE*camera.zoom) + camera.offset.y;
-				width = (int) TILE_SIZE*camera.zoom + 1;
-				blitSprite(renderer, sprites, texture, 2, 25, x, y, width);	//Blits sea underneath coast
-			}
 				//Corner
 			if(i == 0 && j == 0){
 				sprite.x = 1;	//Left top corner
@@ -97,10 +111,20 @@ void dispMap(SDL_Window * window, SDL_Renderer * renderer, SDL_Surface * sprites
 			x = (int) i*(TILE_SIZE*camera.zoom) + camera.offset.x;
 			y = (int) j*(TILE_SIZE*camera.zoom) + camera.offset.y;
 			width = (int) TILE_SIZE*camera.zoom + 1;	//The +1 avoids 1 pixel gap between tiles on particular zoom values
-			blitSprite(renderer, sprites, texture, sprite.x, sprite.y, x, y, width);
+
+			if( (x+width>0 && y+width>0) || (x<SCREEN_WIDTH && y<SCREEN_HEIGHT) ){	//if statement avoids blitting sprites out of the screen
+				blitSprite(renderer, sprites, texture, sprite.x, sprite.y, x, y, width);
+			}
         }
     }
     SDL_RenderPresent(renderer);
+}
+
+
+
+void dispMap(SDL_Window * window, SDL_Renderer * renderer, SDL_Surface * sprites, SDL_Texture * texture, view camera){
+	dispBackground(window, renderer, sprites, texture, camera);
+	dispContinent(window, renderer, sprites, texture, camera);
 }
 
 
@@ -128,14 +152,14 @@ int cameraEvents(SDL_Event * event, view * camera){	//Changes camera's zoom & of
 			camera->offset.y += event->motion.yrel;
 
 			//Map can't go out of the window
-			if(camera->offset.x < -50*camera->zoom*MAP_SIZE)
-				camera->offset.x = -50*camera->zoom*MAP_SIZE;
-			if(camera->offset.x > 1920)
-				camera->offset.x = 1920;
-			if(camera->offset.y < -50*camera->zoom*MAP_SIZE)
-				camera->offset.y = -50*camera->zoom*MAP_SIZE;
-			if(camera->offset.y > 1080)
-				camera->offset.y = 1080;
+			if(camera->offset.x < -TILE_SIZE*camera->zoom*MAP_SIZE)
+				camera->offset.x = -TILE_SIZE*camera->zoom*MAP_SIZE;
+			if(camera->offset.x > SCREEN_WIDTH)
+				camera->offset.x = SCREEN_WIDTH;
+			if(camera->offset.y < -TILE_SIZE*camera->zoom*MAP_SIZE)
+				camera->offset.y = -TILE_SIZE*camera->zoom*MAP_SIZE;
+			if(camera->offset.y > SCREEN_HEIGHT)
+				camera->offset.y = SCREEN_HEIGHT;
 
 			//Prevents map to keep moving since x/yrel are preserved at end of event
 			event->motion.xrel = 0;
@@ -146,7 +170,7 @@ int cameraEvents(SDL_Event * event, view * camera){	//Changes camera's zoom & of
 		if(event->type == SDL_MOUSEWHEEL){
 			newEvent = 1;
 
-			camera->zoom += event->wheel.y * 0.25;
+			camera->zoom += event->wheel.y * ZOOM_FACTOR;	//ZOOM_FACTOR Defines the speed of zoom
 			event->wheel.y = 0;	//Idem
 
 			//Zoom must be between 0.2 and 5
