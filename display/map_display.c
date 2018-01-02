6a3c44e6e5ff287a9f4f2ed91932fb40f797f328
Aurélien Delval <32074290+PurplePachyderm@users.noreply.github.com>
2018-01-02 17:37:55 +0100

    Put coasts outside the map

--- display/map_display.c
+++ display/map_display.c
@@ -41,8 +41,8 @@ void dispBackground(SDL_Window * window, SDL_Renderer * renderer, SDL_Surface * 
 			width = (int) TILE_SIZE*camera.zoom + 1;	//The +1 avoids 1 pixel gap between tiles on particular zoom values
 
 				//if statement avoids blitting sprites under the continent
-			if( (currentPos.x < camera.offset.x+width || currentPos.x > camera.offset.x+(MAP_SIZE-2)*width) ||
-			((currentPos.y < camera.offset.y+width || currentPos.y > camera.offset.y+(MAP_SIZE-2)*width)) ){
+			if( (currentPos.x < camera.offset.x+width || currentPos.x > camera.offset.x+(MAP_SIZE)*width) ||
+			((currentPos.y < camera.offset.y+width || currentPos.y > camera.offset.y+(MAP_SIZE)*width)) ){
 				blitSprite(renderer, sprites, texture, 2, 25, currentPos.x, currentPos.y, width);
 			}
 
@@ -61,23 +61,23 @@ void dispContinent(SDL_Window * window, SDL_Renderer * renderer, SDL_Surface * s
 
 		//Draw tiles
 	coord sprite;	//Defines which sprite is gonna be used (see sprites reference sheet)
-	for(int i = 0; i<MAP_SIZE; i++){
-        for(int j = 0; j<MAP_SIZE; j++){
+	for(int i = 0; i<MAP_SIZE+2; i++){
+        for(int j = 0; j<MAP_SIZE+2; j++){
 
 				//Corner
 			if(i == 0 && j == 0){
 				sprite.x = 1;	//Left top corner
 				sprite.y = 29;
 			}
-			else if(i == MAP_SIZE-1 && j == 0){
+			else if(i == MAP_SIZE+1 && j == 0){
 				sprite.x = 3;	//Right top corner
 				sprite.y = 29;
 			}
-			else if(i == 0 && j == MAP_SIZE-1){
+			else if(i == 0 && j == MAP_SIZE+1){
 				sprite.x = 1;	//Left bottom corner
 				sprite.y = 31;
 			}
-			else if(i == MAP_SIZE-1 && j == MAP_SIZE-1){
+			else if(i == MAP_SIZE+1 && j == MAP_SIZE+1){
 				sprite.x = 3;	//Right bottom corner
 				sprite.y = 31;
 			}
@@ -86,11 +86,11 @@ void dispContinent(SDL_Window * window, SDL_Renderer * renderer, SDL_Surface * s
 				sprite.x = 2;	//Top coast
 				sprite.y = 29;
 			}
-			else if(i == MAP_SIZE-1){
+			else if(i == MAP_SIZE+1){
 				sprite.x = 3;	//Right coast
 				sprite.y = 30;
 			}
-			else if(j == MAP_SIZE-1){
+			else if(j == MAP_SIZE+1){
 				sprite.x = 2;	//Bottom coast
 				sprite.y = 31;
 			}
@@ -153,12 +153,12 @@ int cameraEvents(SDL_Event * event, view * camera){	//Changes camera's zoom & of
 			camera->offset.y += event->motion.yrel;
 
 			//Map can't go out of the window
-			if(camera->offset.x < -TILE_SIZE*camera->zoom*MAP_SIZE)
-				camera->offset.x = -TILE_SIZE*camera->zoom*MAP_SIZE;
+			if(camera->offset.x < -TILE_SIZE*camera->zoom*(MAP_SIZE+2))
+				camera->offset.x = -TILE_SIZE*camera->zoom*(MAP_SIZE+2);
 			if(camera->offset.x > SCREEN_WIDTH)
 				camera->offset.x = SCREEN_WIDTH;
-			if(camera->offset.y < -TILE_SIZE*camera->zoom*MAP_SIZE)
-				camera->offset.y = -TILE_SIZE*camera->zoom*MAP_SIZE;
+			if(camera->offset.y < -TILE_SIZE*camera->zoom*(MAP_SIZE+2))
+				camera->offset.y = -TILE_SIZE*camera->zoom*(MAP_SIZE+2);
 			if(camera->offset.y > SCREEN_HEIGHT)
 				camera->offset.y = SCREEN_HEIGHT;
 
